65a00bc5a916d138b6850e825e70266b2f25d24a
jimenglish81 <jimenglish81@gmail.com>
2015-11-11 21:57:27 +0000

    Initial commit

---
+++ test/mixin.test.js
@@ -0,0 +1,44 @@
+import mixin from '../lib/mixin';
+class Foo {
+  constructor() {
+    this.bar = true;
+  }
+}
+
+describe('Mixin', () => {
+  it('mixes in functionality', () => {
+    let Mixed = mixin(Foo, {
+      baz: true,
+      qux() {
+        return 'qux';
+      }
+    });
+    let foo = new Mixed();
+
+    chai.assert.equal(foo.bar, true);
+    chai.assert.equal(foo.baz, true);
+    chai.assert.equal(foo.qux(), 'qux');
+  });
+
+  it('mixes in multiple functionality', () => {
+    let Mixed = mixin(Foo, {
+        baz: true,
+        qux() {
+          return 'qux';
+        }
+      },
+      {
+        foo: false,
+        qux() {
+          return 'not qux';
+        }
+      }
+    );
+    let foo = new Mixed();
+
+    chai.assert.equal(foo.bar, true);
+    chai.assert.equal(foo.baz, true);
+    chai.assert.equal(foo.foo, false);
+    chai.assert.equal(foo.qux(), 'not qux');
+  });
+});
